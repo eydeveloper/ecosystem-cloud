9918c92799150f823bafda9e0bfa57c6cbe5d5fa
eydeveloper <yaschenko.e02@gmail.com>
2022-07-06 18:40:26 +0300

    Added authorization

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const Router = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const config = require('config');
 const jwt = require('jsonwebtoken');
 const {check, validationResult} = require('express-validator');
 const router = new Router();
@@ -44,5 +45,39 @@ router.post(
     }
   }
 );
+
+router.post('/login', async (req, res) => {
+  try {
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+
+    if (!user) {
+      return res.status(404).json({message: 'Не найден аккаунт с данным адресом эл. почты'});
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(404).json({message: 'Неверный пароль'});
+    }
+
+    const token = jwt.sign({id: user.id}, config.get('secretKey'), {expiresIn: '1h'});
+
+    return res.json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        diskSpace: user.diskSpace,
+        usedSpace: user.usedSpace,
+        avatar: user.avatar
+      }
+    })
+  } catch (e) {
+    console.log(e);
+    res.send({message: 'Произошла ошибка на сервере'});
+  }
+});
+
 
 module.exports = router;
